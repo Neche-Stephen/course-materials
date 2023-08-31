c813e0c42341a33f18b90dec10819203da4057cf
Chinecherem <chinecheremubawike@gmail.com>
2023-08-31 13:22:31 +0100

    Adjusted the navbar title, by centering it

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -10,26 +10,29 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function Navbar({handleShow}) {
   return (
-    <nav className='container-fluid navbar-menu'>
-    <Row className='navbar-row'>
-        <Col className='navbar-logo' xs = '3'>
-            <div className='d-flex align-items-center navbar-height'>
-                <Link to='/'><img className='img-fluid' src={RAD5_LOGO} alt="" /></Link>
-            </div>
-        
-        </Col>
-
-        <Col className="d-lg-none" xs = '1'>
-              <div className='d-flex align-items-center navbar-height'><button onClick={handleShow} className="bi bi-justify"></button></div>
-        </Col>
-
-        <Col xs = '8' sm ='6' lg ='9' className='p-0' >
-                <div className='navbar-title navbar-height d-flex justify-content-center align-items-center'>
-                      <h1 style={{whiteSpace:'nowrap'}}> <span className='d-none d-sm-inline'>RAD5</span> StudySphere</h1>
+    <>
+      <nav className='container-fluid navbar-menu'>
+        <Row className='navbar-row'>
+            <Col className='navbar-logo' xs = '3'>
+                <div className='d-flex align-items-center navbar-height'>
+                    <Link to='/'><img className='img-fluid' src={RAD5_LOGO} alt="" /></Link>
                 </div>
-        </Col>
-        
-    </Row>
-  </nav>
+            </Col>
+
+            <Col className="d-lg-none" xs = '2'>
+                  <div className='d-flex align-items-center navbar-height'><button onClick={handleShow} className="bi bi-justify"></button></div>
+            </Col>
+
+            <Col   xs = '6' sm ='6' lg ='9' className='' >
+                    <div className='navbar-title navbar-height d-flex justify-content-center align-items-center'>
+                          <h1 style={{whiteSpace:'nowrap'}}> <span className='d-none d-sm-inline'>RAD5</span>StudySphere</h1>
+                    </div>
+            </Col>
+            
+        </Row>
+      </nav>
+
+
+    </>
   )
 }
