f127931c45ceafc06154177036c0a2331c1819e0
Chinecherem <chinecheremubawike@gmail.com>
2023-09-13 11:14:21 +0100

    Added materials for the topic Datatypes

--- src/component/jsTopics/DataTypes/DataTypes.jsx
+++ src/component/jsTopics/DataTypes/DataTypes.jsx
@@ -8,23 +8,30 @@ const topicDetails = {
   learningObjective : '',
   lectureMaterialLink : [
       {
-          materialName : 'Introduction to Javascript',
-          materialLink : "https://docs.google.com/document/d/18AP9H_O6Upr_CvkiNDW_goPLYIxcLi0Vb1hggtjKfOw/edit?usp=sharing",
+          materialName : 'Data Types',
+          materialLink : "https://docs.google.com/document/d/1cEKdEdeKQhaBdYijX-nF05x8q975Xt0eFSydAZnZ3Xc/edit?usp=sharing",
       }
   ],
   ResourcesLink : [
-      {
-          resourceLink : "https://javascript.info/variables",
-          resourceName : 'Variables'
-      },
+      // {
+      //     resourceLink : "https://javascript.info/variables",
+      //     resourceName : 'Variables',
+      //     resourceInfo : 'Attempt the tasks at the end of this Resource.'
+      // },
       ]
+}
+
+const topicAssessments = {
+  exercise : 'https://docs.google.com/document/d/1hBhsbaZFrBMFrTWBgF_l8yZQYczROQbr-I2hpmzQMSA/edit?usp=sharing',
+  classwork : 'https://docs.google.com/document/d/1wh38gmaqezzfTQueJMP9WnWA-1F6-D_hep928c7NEQw/edit?usp=sharing',
+  homework : 'https://docs.google.com/document/d/1fvEAjdfVeKg6_swvuRxSgsPZW6azKtXOfzv0G0VBIYE/edit?usp=sharing'
 }
 
 export default function DataTypes() {
   return (
     <div>
-        {/* <TopicComponent topicDetails={topicDetails}/> */}
-        No Lecture Material Available for this topic
+          <TopicComponent topicDetails = {topicDetails} topicAssessments = {topicAssessments} />
+        {/* No Lecture Material Available for this topic */}
     </div>
   )
 }
