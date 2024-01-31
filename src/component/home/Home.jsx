361317b74eadcb48d8b7ec497d5d9e220acb5418
Chinecherem <chinecheremubawike@gmail.com>
2024-01-31 09:30:35 +0100

    New changes

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className='mt-3' style={{fontFamily:'Montserrat'}}>
-        <h1>Welcome to StudySphere</h1>
+        <h1></h1>
         <p>Use the Side bar to view each topic and materials under the topic</p>
         <p>If you are on a mobile device, Click on the menu icon to see the sidebar</p>
     </div>
