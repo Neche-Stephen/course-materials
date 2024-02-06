2bb9da62ed128e1b3901866fb71a586840b2c3d0
Chinecherem <chinecheremubawike@gmail.com>
2024-02-06 14:28:14 +0100

    New changes

--- src/component/css-topics/IntroCss/Intro-css.jsx
+++ src/component/css-topics/IntroCss/Intro-css.jsx
@@ -7,24 +7,33 @@ const topicDetails = {
   learningObjective : '',
   lectureMaterialLink : [
       {
-          materialName : 'Introduction to Javascript',
-          materialLink : "https://docs.google.com/document/d/18AP9H_O6Upr_CvkiNDW_goPLYIxcLi0Vb1hggtjKfOw/edit?usp=sharing",
-      }
+          materialName : 'Introduction to CSS',
+          materialLink : "https://docs.google.com/document/d/1cKmdrFsWooIQM3Jk5AZSKy_WcPX83RhS/edit?usp=sharing&ouid=106772511003676051114&rtpof=true&sd=true",
+      },
+      {
+        materialName : 'Block Level, Inline and Inline-block Elements',
+        materialLink : "https://docs.google.com/document/d/1DU-oNMdFE_xMJGSczCl1eYP7hymbqd0mD3CT-QWR7sA/edit?usp=sharing",
+    }
   ],
   ResourcesLink : [
-      {
-          resourceLink : "https://javascript.info/variables",
-          resourceName : 'Variables'
-      },
+      // {
+      //     resourceLink : "https://javascript.info/variables",
+      //     resourceName : 'Variables'
+      // },
       ]
+}
+
+const topicAssessments = {
+  exercise : 'https://docs.google.com/document/d/1mYmvj8cboCQ_CvyVPeNWhKKYxeYR9VhaEUaC899xF5I/edit?usp=sharing',
+  classwork : 'https://docs.google.com/document/d/1_OGl2NdoAEQTnFFZ_YQfjXwAjbjHcZxxPTCgAF3GkIg/edit?usp=sharing',
+  homework : 'https://docs.google.com/document/d/1noFDa7GIy5gA3lwT6pWMDsnRHdRvKMcHFeu_XbxEmPs/edit?usp=sharing'
 }
 
 
 export default function IntroCss() {
   return (
     <div>
-      {/* <TopicComponent topicDetails={topicDetails}/> */}
-      No Lecture Material Available for this topic
+      <TopicComponent topicDetails={topicDetails} topicAssessments = {topicAssessments}/>
     </div>
   )
 }
