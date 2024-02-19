f130ff987f34d6e7ffb562327fcc872e10b43c39
Chinecherem <chinecheremubawike@gmail.com>
2024-02-19 07:39:47 +0100

    New topic - Flex

--- src/component/css-topics/flex/Flex.jsx
+++ src/component/css-topics/flex/Flex.jsx
@@ -21,7 +21,7 @@ const topicDetails = {
 }
 
 const topicAssessments = {
-  exercise : 'https://docs.google.com/document/d/1VP_UvR9wvzooFxVvrY-n1EIy9XlS459g0drDb6cyDFM/edit?usp=sharing',
+  exercise : 'https://docs.google.com/document/d/1VP_UvR9wvzooFxVvrY-n1EIy9XlS459g0drDb6cyDFM/edit?usp=sharing.',
   classwork : 'https://docs.google.com/document/d/178I5l8N-XXcwtpkkKevbnDkpvmon01wJDOxUHmMW8k8/edit?usp=sharing',
   homework : 'https://docs.google.com/document/d/1HJJa9d7QADFv3Y819xxEdWtXNG1zxHDV-9CPWTCT0Ms/edit?usp=sharing'
 }
